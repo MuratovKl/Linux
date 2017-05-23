0fde89aad0378771594a537deb24b016c3921f95
MuratovKl <muratovkirill@outlook.com>
2017-05-23 23:56:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<php 
-	phpinfo();	
-?>
+<?php header('refresh: 15'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>sysi</title>
+</head>
+<body>
+<h1>Load Average</h1>
+<?php echo shell_exec(cat /proc/loadavg | awk '{print $1" "$2" "$3}'); ?>
+
+</body>
+</html>
